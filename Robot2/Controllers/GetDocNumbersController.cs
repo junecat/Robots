ffc24ee61fbfa909c287786aa4590cedef4e93cb
Konst <junecat@gmail.com>
2022-02-24 00:20:48 +0300

    Болванка с контроллером и без бизнес-логики вообще

---
+++ Robot2/Controllers/GetDocNumbersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Serilog;
+using System.Threading;
+
+namespace GetDocNumbers.Controllers {
+
+    [ApiController]
+    [Route("[controller]")]
+    public class GetDocNumbersController : ControllerBase {
+
+        [HttpGet]
+        public async Task<IActionResult> Get() {
+            Log.Information($"GetDocNumbersController.Get() called");
+
+
+            
+            await Task.Run ( () => TestSyncMethod() );
+
+            return Ok("Hi from Get!");
+        
+        }
+
+        private string TestSyncMethod() {
+            // Do stuff    
+            Thread.Sleep(5000);
+
+            return "55";
+        }
+
+    }
+
+}
---
+++ Robot2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
+using System.Net;
+
+namespace GetDocNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("Logs/GetDocNumbersRobot_.log", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+            Log.Information(new string('=', 60));
+            Log.Information("Running GetDocNumbersRobot ver 0.1");
+
+            var host = new WebHostBuilder().UseKestrel(GetKso()).UseContentRoot(Directory.GetCurrentDirectory()).UseStartup<Startup>().UseWebRoot("static").Build();
+            host.Run();
+
+        }
+
+        static Action<KestrelServerOptions> GetKso() {
+            Action<KestrelServerOptions> ret = options =>
+            {
+                options.Limits.MaxConcurrentConnections = 100;
+                options.Listen(IPAddress.Any, 5002);
+            };
+            return ret;
+        }
+
+    }
+}
